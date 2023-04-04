72c297e34d7739d08091c206702ff09f50b231a0
kylekuminkoski <krk0020@mix.wvu.edu>
2023-04-04 12:54:05 -0400

    first commit

---
+++ survey1-python.py
@@ -0,0 +1,24 @@
+#Returns the factioral of the parameter 'n' by recursively
+#multiplying 'n' by the factorial of 'n-1' until 'n-1' equals 1.
+#Default return value of 1 for all 'n' values less than or equal to 1
+def factorial(n):
+    if (n <= 1):
+        return 1
+    else:
+        return n * (factorial(n-1))
+
+
+
+def combinations(n, r):
+    if(0 <= r and r >= n):
+        return 0
+    else:
+        return factorial(n) / (factorial(r) * factorial(n-r))
+
+def hypercake(n, k):
+    value = 0
+    while(k >= 0):
+        value = value + combinations(n, k)
+        k = k - 1
+    return value
+
